6cb9805272b57b8c9b061cdbcb94772e6da6da03
Geert De Deckere <geertdd@gmail.com>
2012-10-10 16:46:36 +0200

    Adding some styles

--- single-post.php
+++ single-post.php
@@ -2,8 +2,16 @@
 <?php the_post() ?>
 
 <article <?php post_class() ?>>
-	<h1><?php the_title() ?></h1>
-	<?php the_content() ?>
+	<header>
+		<h1><?php the_title() ?></h1>
+		<p class="date"><?php the_date() ?></p>
+	</header>
+	<section>
+		<?php the_content() ?>
+	</section>
+	<section>
+		<p>Comments</p>
+	</section>
 </article>
 
 <?php get_footer() ?>
