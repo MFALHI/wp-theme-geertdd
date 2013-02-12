a981c47ec57239e7047a41fc286a75c503867842
Geert De Deckere <geertdd@gmail.com>
2013-02-12 10:24:13 +0100

    Prism syntax highlighter functionality should be moved to a plugin

--- js/geertdd.js
+++ js/geertdd.js
@@ -1,15 +1,5 @@
 (function ($) {
 
-	// Load syntax highlighter if needed
-	Modernizr.load({
-		test: $('code[class*="language-"]').length,
-		yep: [
-			GEERTDD.theme_url + '/css/prism.css',
-			GEERTDD.theme_url + '/js/prism.js'
-		],
-		complete: function () {
-			Prism.highlightAll();
-		}
-	});
+	'use strict';
 
 }(jQuery));
